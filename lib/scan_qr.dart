b81d433483e1dac67578208970e954ffae7ccfdc
jicsontoh <jicsontoh@gmail.com>
2023-05-29 22:35:36 +0800

    Update QR scanner

--- lib/scan_qr.dart
+++ lib/scan_qr.dart
@@ -1,6 +1,8 @@
+
 import 'dart:typed_data';
 
 import 'package:ecomatesg/Icons/change_color.dart';
+import 'package:ecomatesg/home_page.dart';
 import 'package:ecomatesg/scan_qr.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +12,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
+import 'FirebaseHelper/firestore.dart';
 import 'main.dart';
 
 class ScanQRPage extends StatefulWidget {
@@ -21,8 +24,17 @@ class ScanQRPage extends StatefulWidget {
 
 class _ScanQRPageState extends State<ScanQRPage> {
   static const textColour = Color.fromARGB(255, 44, 82, 105);
+  static const QR_CODE = "--ecomatesg--pleaseletuswin--";
   double deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
   double deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
+
+  Future<void> _addPlasticBagPoints(int pointsToAdd) async {
+    await FirestoreCollectionHelper.addPlasticBagPoints(pointsToAdd);
+  }
+
+  Future<void> _addTransportPoints(int pointsToAdd) async {
+    await FirestoreCollectionHelper.addTransportPoints(pointsToAdd);
+  }
 
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
@@ -42,10 +54,57 @@ class _ScanQRPageState extends State<ScanQRPage> {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
       if (scanData != null) {
-        setState(() {
-          result = scanData;
-        });
-        print(scanData.code);
+        if (scanData.code?.substring(0, 29) == QR_CODE) {
+          dispose();
+          setState(() {
+            result = scanData;
+          });
+          var res = scanData.code?.substring(29);
+          _addPlasticBagPoints(int.parse(res!));
+          
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Congratulations!',
+                  style: TextStyle(color: textColour,
+                      fontFamily: 'NotoSans')),
+              content: Text("You have gained ${res} points",
+                  style: const TextStyle(color: textColour,
+                      fontFamily: 'NotoSans')),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const HomePage()),
+                  ),
+                  child: const Text('Back home'),
+                ),
+              ],
+            ),
+          );
+          print(scanData.code);
+        } else {
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Sorry!',
+                  style: TextStyle(color: textColour,
+                      fontFamily: 'NotoSans')),
+              content: const Text('Invalid QR Code',
+                  style: TextStyle(color: textColour,
+                      fontFamily: 'NotoSans')),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const HomePage()),
+                  ),
+                  child: const Text('Back home'),
+                ),
+              ],
+            ),
+          );
+
+        }
+
       }
     });
   }
@@ -140,3 +199,32 @@ class _ScanQRPageState extends State<ScanQRPage> {
 
 
 }
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Show Dialog'),
+    );
+  }
+}
+
